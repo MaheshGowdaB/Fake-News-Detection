0a471c6ac20f284863f7f09a54dac65a87a69dce
Mahesh <maheshgowda3501@gmail.com>
2023-07-13 05:44:09 +0530

    commit

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from flask import Flask, render_template, request
+import matplotlib.pyplot as plt
+import io
+import base64
 import numpy as np
 import pandas as pd
 import re
+from flask import Flask, render_template, request
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, confusion_matrix, roc_curve, auc
-from sklearn.model_selection import learning_curve, train_test_split
-import matplotlib.pyplot as plt
-import seaborn as sns
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import learning_curve
 import nltk
 
 nltk.download('stopwords')
@@ -20,7 +21,7 @@ app = Flask(__name__)
 stop_words = stopwords.words('english')
 
 # Load the news dataset
-news_dataset = pd.read_csv('C:/Users/LENOVO/Documents/Fake News Detection/train.csv')
+news_dataset = pd.read_csv('C:/Users/LENOVO/Documents/Fake_News_Detection/train.csv')
 news_dataset = news_dataset.fillna('')
 
 # Create a new column 'content' by combining 'author' and 'title'
@@ -47,53 +48,9 @@ news_dataset['content'] = news_dataset['content'].apply(stemming)
 vectorizer = TfidfVectorizer()
 X = vectorizer.fit_transform(X)
 
-# Split the data into training and testing sets
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, stratify=Y, random_state=2)
-
 # Create and train the logistic regression model
 model = LogisticRegression()
-model.fit(X_train, Y_train)
-
-# Predict the labels for the test set
-Y_pred = model.predict(X_test)
-
-# Calculate the accuracy of the model
-accuracy = accuracy_score(Y_test, Y_pred)
-print("Accuracy:", accuracy)
-
-# Generate confusion matrix
-cm = confusion_matrix(Y_test, Y_pred)
-print("Confusion Matrix:")
-print(cm)
-
-# Generate learning curve
-train_sizes, train_scores, test_scores = learning_curve(model, X_train, Y_train, cv=5, scoring='accuracy', train_sizes=np.linspace(0.1, 1.0, 10))
-
-# Calculate the mean and standard deviation of training and testing scores
-train_scores_mean = np.mean(train_scores, axis=1)
-train_scores_std = np.std(train_scores, axis=1)
-test_scores_mean = np.mean(test_scores, axis=1)
-test_scores_std = np.std(test_scores, axis=1)
-
-# Plot the learning curve
-plt.figure(figsize=(10, 6))
-plt.plot(train_sizes, train_scores_mean, 'o-', color='r', label='Training Accuracy')
-plt.plot(train_sizes, test_scores_mean, 'o-', color='g', label='Validation Accuracy')
-plt.fill_between(train_sizes, train_scores_mean - train_scores_std, train_scores_mean + train_scores_std, alpha=0.1, color='r')
-plt.fill_between(train_sizes, test_scores_mean - test_scores_std, test_scores_mean + test_scores_std, alpha=0.1, color='g')
-plt.xlabel('Training Set Size')
-plt.ylabel('Accuracy')
-plt.title('Learning Curve')
-plt.legend(loc='best')
-plt.grid(True)
-plt.savefig('learning_curve.png')
-
-# Save the confusion matrix plot
-sns.heatmap(cm, annot=True, cmap="Blues", fmt="d", xticklabels=['Real', 'Fake'], yticklabels=['Real', 'Fake'])
-plt.xlabel('Predicted Labels')
-plt.ylabel('True Labels')
-plt.title('Confusion Matrix')
-plt.savefig('confusion_matrix.png')
+model.fit(X, Y)
 
 @app.route('/')
 def home():
@@ -105,11 +62,62 @@ def predict():
     stemmed_content = stemming(news_content)
     input_data = vectorizer.transform([stemmed_content])
     prediction = model.predict(input_data)[0]
-    if prediction == 0:
-        prediction_text = 'Real'
-    else:
-        prediction_text = 'Fake'
-    return render_template('index.html', prediction=prediction_text, confusion_matrix='confusion_matrix.png', learning_curve='learning_curve.png', accuracy=accuracy)
+
+    # Generate the confusion matrix
+    train_predictions = model.predict(X)
+    cm = confusion_matrix(Y, train_predictions)
+
+    # Generate the learning curve
+    train_sizes, train_scores, test_scores = learning_curve(model, X, Y, cv=5)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+
+    # Plot the confusion matrix
+    plt.figure(figsize=(8, 6))
+    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+    plt.title('Confusion Matrix')
+    plt.colorbar()
+    plt.xticks([0, 1], ['Fake', 'Real'])
+    plt.yticks([0, 1], ['Fake', 'Real'])
+    plt.xlabel('Predicted Label')
+    plt.ylabel('True Label')
+    plt.tight_layout()
+
+    # Save the confusion matrix plot to a buffer
+    confusion_matrix_buffer = io.BytesIO()
+    plt.savefig(confusion_matrix_buffer, format='png')
+    confusion_matrix_buffer.seek(0)
+
+    # Convert the buffer to a base64 encoded string
+    confusion_matrix_image = base64.b64encode(confusion_matrix_buffer.read()).decode('utf-8')
+
+    # Plot the learning curve
+    plt.figure(figsize=(8, 6))
+    plt.title('Learning Curve')
+    plt.xlabel('Training Examples')
+    plt.ylabel('Score')
+    plt.grid()
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1, color='r')
+    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1, color='g')
+    plt.plot(train_sizes, train_scores_mean, 'o-', color='r', label='Training Score')
+    plt.plot(train_sizes, test_scores_mean, 'o-', color='g', label='Cross-Validation Score')
+    plt.legend(loc='best')
+
+    # Save the learning curve plot to a buffer
+    learning_curve_buffer = io.BytesIO()
+    plt.savefig(learning_curve_buffer, format='png')
+    learning_curve_buffer.seek(0)
+
+    # Convert the buffer to a base64 encoded string
+    learning_curve_image = base64.b64encode(learning_curve_buffer.read()).decode('utf-8')
+
+    return render_template('index.html', prediction=prediction,
+                           confusion_matrix_image=confusion_matrix_image,
+                           learning_curve_image=learning_curve_image)
 
 if __name__ == '__main__':
     app.run(debug=True)
